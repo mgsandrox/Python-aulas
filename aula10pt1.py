5a14fe0666db44664621cbc58b6204b0571570a7
Alessandro Tavares <80715104+mgsandrox@users.noreply.github.com>
2022-07-19 21:36:58 -0300

    Condições (Parte 1)

---
+++ aula10pt1.py
@@ -0,0 +1,22 @@
+'''tempo =  int(input('Quantos anos tem o seu veículo? '))
+if tempo <=7:
+    print('Veículo novo!')
+else:
+    print('Já está na hora de trocar né chefia?')
+    print('Até proxima velhinho!!')'''
+
+'''tempo = int(input("Quantos anos tem sua moto? "))
+print('Uma moto novinha' if tempo <=9 else 'Ela já viu dias melhores')
+print('E por hoje isso é tudo pessoal!')'''
+
+n1 =  float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+m = (n1 + n2)/2
+print("Parabéns" if m >=6 else 'Perdeu')
+print(f'Sua média foi {m:.1f}')
+'''if m >= 6:
+    print("Meus parabens você foi muito bem!")
+else:
+    print("Você tentou, mas não foi o sufuciênte...")'''
+
+
