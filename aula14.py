749c0201601b2327c5bdb403521a93b92ad035a4
Alessandro Tavares <80715104+mgsandrox@users.noreply.github.com>
2022-08-31 20:34:37 -0300

    atualização

--- aula14.py
+++ aula14.py
@@ -1,3 +1,5 @@
+
+
 '''n = 1
 while n != 0: Flag: é um ponto deparada
     n = int(input('Digite um valor: '))
@@ -10,7 +12,7 @@ while n != 0: Flag: com outra condicinal
     r = str(input('Deseja continuar? [S/N]')).upper()
 print('FIM')'''
 
-n= 1
+'''n= 1
 par = impar = 0
 while n != 0:
     n = int(input('Digite um valor: '))
@@ -19,4 +21,29 @@ while n != 0:
             par += 1
         else:
             impar += 1
-print(f'Você digitou {par} números pares e {impar} números ímpares!')
+print(f'Você digitou {par} números pares e {impar} números ímpares!')'''
+
+'''#Loop com uma lista
+carros = ["HRV", "Gol", "Agro", "Poche", "Fusca"]
+i = 0;
+tam = len(carros)
+
+while  i < tam:
+    print(carros[i])
+    i += 1
+print("\nFim do loop")'''
+
+#Loop com lista e um for, buscando a informação do usuario
+import os
+
+carros = []
+carro = input("Informe um modelo de carro: ")
+
+while  carro != "-1":
+    carros.append(carro)
+    carro = input('Informe outro modelo: ')
+
+os.system('cls')
+for x in carros:
+    print(x)
+print("\nFim do loop")
